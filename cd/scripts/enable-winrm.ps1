a8d6aeb9ec12d411aa5fafdbbb703fb11e2a8aef
Aaron Rombaut <aaron.rombaut@gmail.com>
2026-05-03 09:32:18 -0400

    More access to allow WinRM to run

--- cd/scripts/enable-winrm.ps1
+++ cd/scripts/enable-winrm.ps1
@@ -1,10 +1,14 @@
-# Set all networks to Private
+# Set network to Private
 Get-NetConnectionProfile | ForEach-Object {
   Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Private
 }
 
-# Enable WinRM
-Enable-PSRemoting -Force
+# Enable WinRM fully
+winrm quickconfig -quiet
+
+# Allow local admin remote access
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
+  -Name "LocalAccountTokenFilterPolicy" -Value 1 -PropertyType DWord -Force
 
 # Ensure service
 Set-Service -Name WinRM -StartupType Automatic
