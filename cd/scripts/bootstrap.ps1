915e6fa98b885924594956f78df054f82d5aea75
Aaron Rombaut <aaron.rombaut@gmail.com>
2026-05-03 10:18:20 -0400

    Changing method to enable WinRM to PS script and autounattend.xml

--- cd/scripts/bootstrap.ps1
+++ cd/scripts/bootstrap.ps1
@@ -1,10 +1,20 @@
-# bootstrap.ps1
+# Enable WinRM
+Get-NetConnectionProfile | ForEach-Object {
+  Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Private
+}
 
-# Install VMware Tools (from attached ISO)
-$cd = Get-Volume | Where-Object {
+winrm quickconfig -quiet
+
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
+  -Name "LocalAccountTokenFilterPolicy" -Value 1 -PropertyType DWord -Force
+
+Enable-NetFirewallRule -DisplayGroup "Windows Remote Management"
+
+# Install VMware Tools
+$tools = Get-Volume | Where-Object {
   $_.DriveType -eq 'CD-ROM' -and (Test-Path "$($_.DriveLetter):\setup64.exe")
 } | Select-Object -First 1
 
-if (-not $cd) { throw "VMware Tools ISO not found" }
-
-Start-Process "$($cd.DriveLetter):\setup64.exe" -ArgumentList '/S /v"/qn REBOOT=R"' -Wait
+if ($tools) {
+  Start-Process "$($tools.DriveLetter):\setup64.exe" -ArgumentList '/S /v"/qn REBOOT=R"' -Wait
+}
