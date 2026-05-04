f9a8c6afdd87d8b36cff288bade36b514aa62d2b
Aaron Rombaut <aaron.rombaut@gmail.com>
2026-05-04 16:10:31 -0400

    New script to enable WinRM

--- cd/scripts/enable-winrm.ps1
+++ cd/scripts/enable-winrm.ps1
@@ -1,17 +1,31 @@
-Start-Sleep -Seconds 5
-
+# Force all network profiles to Private
 Get-NetConnectionProfile | ForEach-Object {
-  Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Private -ErrorAction SilentlyContinue
+    Set-NetConnectionProfile -InterfaceIndex $_.InterfaceIndex -NetworkCategory Private -ErrorAction SilentlyContinue
 }
 
-Start-Sleep -Seconds 5
+# Enable WinRM listener + firewall rules
+Enable-PSRemoting -Force
 
-winrm quickconfig -quiet
+# Ensure WinRM service
+Set-Service WinRM -StartupType Automatic
+Start-Service WinRM
 
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
-  -Name "LocalAccountTokenFilterPolicy" -Value 1 -PropertyType DWord -Force
+# Allow local Administrator over remote UAC
+New-ItemProperty `
+  -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" `
+  -Name "LocalAccountTokenFilterPolicy" `
+  -Value 1 `
+  -PropertyType DWord `
+  -Force
 
+# Allow Basic/unencrypted for Packer HTTP WinRM build use
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+
+# Explicit firewall rules
 Enable-NetFirewallRule -DisplayGroup "Windows Remote Management"
+Enable-NetFirewallRule -DisplayGroup "File and Printer Sharing"
 
-winrm set winrm/config/service/auth @{Basic="true"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
+# Verify
+winrm enumerate winrm/config/listener
+netstat -an | findstr 5985
